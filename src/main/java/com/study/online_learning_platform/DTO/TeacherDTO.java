f6fef9e792d5a74334f6fdd963bfeb1769b1f670
BuiNgocThach <iambnthach@gmail.com>
2025-07-11 22:42:13 +0700

    feat(project): create base project

---
+++ src/main/java/com/study/online_learning_platform/DTO/TeacherDTO.java
@@ -0,0 +1,12 @@
+package com.study.online_learning_platform.DTO;
+
+public class TeacherDTO {
+    // Declare variables
+    private String name;
+    private String major;
+    private String introduction;
+
+    // Getters and Setters
+    // End Getters and Setters
+
+}
